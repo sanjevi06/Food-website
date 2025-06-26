1e38ee6bd7482c5a7b710223a9160e908444b0c9
sanjevi06 <sanjeevirengarajan@gmail.com>
2025-06-26 18:07:13 +0530

    worked on script

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const himage =document.querySelector("#himg img");
+const dishes =document.querySelectorAll(".dish");
+const cartcount =document.querySelector(".cart-count");
+const addtocart =document.querySelectorAll(".add-to-cart");
+const bar =document.getElementById("Burger");
+const navbar =document.getElementById("nav")
+let cart =[];
 
+dishes.forEach(dish =>{
+    dish.addEventListener("click", () => {
+        const newimg =dish.getAttribute("data-img");
+        himage.src =newimg;
+        
+
+    })
+})
+
+bar.addEventListener("click", () => {
+navbar.classList.toggle("active");
+
+})
+
+
+addtocart.forEach(button => {
+    button.addEventListener("click" , (e)=>{
+        e.stopImmediatePropagation();
+        const name =button.getAttribute("data-name")
+        const price =parseFloat(button.getAttribute("data-price"))
+        cart.push({name,price});
+        updatecartcount();
+    })
+})
+
+function updatecartcount (){
+cartcount.textContent=cart.length;
+
+}
