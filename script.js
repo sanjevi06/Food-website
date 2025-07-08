5db7be2f2b53c2d087b97c5e1229c8f6066fca62
sanjevi06 <sanjeevirengarajan@gmail.com>
2025-07-08 21:23:57 +0530

    Worked on form validation

--- script.js
+++ script.js
@@ -46,6 +46,57 @@ let items  =[
          Image:"images/food-5.png",
         price:2.00
     },
+    {
+        id:5,
+        name:"Pizza",
+        Image:"images/food-6 (2).png",
+        price:3.00
+    },
+     {
+        id:6,
+        name:"wing",
+        Image:"images/food-7.png",
+        price:3.00
+    },
+    {
+        id:7,
+        name:"Briyani",
+        Image:"images/food-8.png",
+        price:2.00
+    },
+      {
+        id:8,
+        name:"Meal",
+        Image:"images/food-9.png",
+        price:3.00
+    },
+      {
+        id:9,
+        name:"BurgerCombo",
+        Image:"images/food-10.png",
+        price:3.00
+    },
+      {
+        id:10,
+        name:"Pizza",
+        Image:"images/food-11.png",
+        price:2.00
+    },
+     {
+        id:11,
+        name:"Noodles",
+        Image:"images/food-12.png",
+        price:2.00
+    },
+
+         {
+        id:12,
+        name:"Sushi",
+        Image:"images/food-13.png",
+        price:4.00
+    },
+     
+
 ];
  
 var productbox =document.querySelector(".dishes")
@@ -95,9 +146,10 @@ productli.innerHTML=`  <img src="${item.Image}">
                 <button onClick="changecount(${key},${item.cartcount - 1})">-</button>
                 <span>${item.cartcount}</span>
                 <button onClick="changecount(${key},${item.cartcount + 1})">+</button>
+                <button class="delete-items" onClick="deleteall(${key})"><i class="fa-solid fa-square-minus"></i></button>
             </div>`
             cartlist.append(productli)
-
+ 
     })
     cartcount.style.display="block"
     cartcount.innerHTML=count;
@@ -113,5 +165,193 @@ else{
 }
 reloadproduct()
 }
+ function deleteall(key){
+        delete storeproduct[key];
+        reloadproduct()
+    }
+
+//login page
+var auth =document.querySelector (".auth")
+var signin =document.querySelector(".btn")//signin button
+var closelogin =document.querySelector(".closelogin")//close button in auth
+
+var loginlink =document.querySelector(".login-link")
+var reglink =document.querySelector(".reg-link") 
+
+// //increase the height of the box by .slide class
+reglink.addEventListener("click", () =>{
+    auth.classList.add("slide");
+})
+loginlink.addEventListener("click", () => {auth.classList.remove("slide")})
+
+// //to show the login page by .open class
+
+signin.addEventListener("click", () =>{
+    auth.classList.add("open");
+})
+    closelogin.addEventListener("click",()=>{
+    auth.classList.remove("open","slide")
+})
 
 
+// // //alert
+var loginbutton =document.querySelector(".loginbtn") //login button in auth
+var alertbox =document.getElementById("succ") //alert checkbox
+
+
+//to show the alert when the validation is crct
+loginbutton.addEventListener("click", ()=>{
+ if(validationLogin()){
+alertbox.classList.add("showit")
+setTimeout(() =>{
+    alertbox.classList.remove("showit")
+},3000)
+ }
+})
+
+// //reg not yet done
+
+
+//to show logout button selecting it
+var logoutbtn=document.getElementById("logout")
+
+// //for closing auth after login and change singin to logout
+loginbutton.addEventListener("click",()=>{
+    if(validationLogin()){
+    auth.classList.toggle("open")
+        // logoutbtn.classList.toggle("logoutshow")
+     logoutbtn.style.display=("block")
+        // signin.classList.toggle("logoutshows")
+          signin.style.display=("none")
+        
+}
+
+})
+
+//logout to sign in
+function signout(){
+    signin.style.display=("block");
+    logoutbtn.style.display=("none")
+    //to refresh the page  
+    location.reload()
+}
+
+//Validation part for login
+var loginform=document.querySelector("#loginform")
+var regform=document.querySelector("#regform")
+var email=document.querySelector("#email")
+var password=document.querySelector("#password")
+
+loginform.addEventListener("submit",(e)=>{
+   //til validation is crct it will not allow to perform action
+    if(!validationLogin()){
+ e.preventDefault()
+    }
+})
+
+function validationLogin(){
+    emailvalue = email.value;
+    passwordval = password.value;
+  
+    
+    
+let crct =true
+    if(emailvalue===''){
+        crct=false
+        seterror(email,'Email is required')
+    }
+    else{
+        setsuccess(email)
+    }
+
+    if(passwordval=== ''){
+         crct=false
+        seterror(password,'Password is required')
+    }
+    else{
+        setsuccess(password)
+    }
+
+
+    return crct
+}
+// validation for register
+var username=document.querySelector("#username")
+var emailreg =document.querySelector("#emailreg")
+var passwordreg =document.querySelector("#passwordreg")
+
+regform.addEventListener("submit",(e)=>{
+   //til validation is crct it will not allow to perform action
+    if(!validationReg()){
+ e.preventDefault()
+    }
+})
+
+function validationReg(){
+  usernameval=username.value.trim();
+    emailregval=emailreg.value;
+    passwordregval=passwordreg.value;
+
+    let crct =true
+
+    
+    if(usernameval== ''){
+        crct=false
+        seterror(username,"username is required")
+    }
+    else{
+        setsuccess(username)
+    }
+if(emailregval== ''){
+    seterror(emailreg,"email is required")
+}
+else{
+    setsuccess(emailreg)
+}
+
+if(passwordregval== ''){
+    seterror(passwordreg,'password is required')
+}
+else{
+    setsuccess(passwordreg)
+}
+return crct
+}
+
+function seterror(element,message){
+    const inputgroup = element.parentElement;
+    const errorelement = inputgroup.querySelector(".error")
+    errorelement.innerText=message;
+    inputgroup.classList.add("error")
+    inputgroup.classList.remove("success")
+    
+
+}
+
+
+function setsuccess(element){
+    const inputgroup = element.parentElement;
+    const errorelement = inputgroup.querySelector(".error")
+    errorelement.innerText='';
+    inputgroup.classList.add("success")
+    inputgroup.classList.remove("error")
+
+}
+
+var regbtn =document.querySelector(".regbtn")
+var alertboxreg=document.querySelector("#regsucc")
+
+
+//when clicking register button in regform
+function clickreg(){
+     if(validationReg()){
+
+auth.classList.remove("open","slide")
+ alertboxreg.classList.add("showiti")
+ setTimeout(() =>{
+    alertboxreg.classList.remove("showiti")
+},3000)
+ 
+     }
+    
+}
